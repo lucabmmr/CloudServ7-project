d452433502028ec661f89f7b0168780c9a777407
Lena111Mx <lena.von.haimbach@gmail.com>
2026-02-11 17:08:06 +0100

    sichtbare Ergbnisse bei Test

--- loadtest/k6-express-web.js
+++ loadtest/k6-express-web.js
@@ -2,10 +2,11 @@ import http from 'k6/http';
 
 export const options = {
   stages: [
-    { duration: '20s', target: 0 },
-    { duration: '30s', target: 150 },
-    { duration: '30s', target: 200 },
-    { duration: '30s', target: 0 },
+    { duration: '20s', target: 20 },
+    { duration: '40s', target: 60 },
+    { duration: '40s', target: 100 },
+    { duration: '40s', target: 150 },
+    { duration: '40s', target: 0 },
   ],
 };
 
