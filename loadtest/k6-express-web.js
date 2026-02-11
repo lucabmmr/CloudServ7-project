e3ecaff44b1ae933c8353b5aa5c7a0e595ced688
Lena111Mx <lena.von.haimbach@gmail.com>
2026-02-11 17:34:39 +0100

    etwas aggresiver , um max anzahl pods zu erreichen

--- loadtest/k6-express-web.js
+++ loadtest/k6-express-web.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '30s', target: 40 },
-    { duration: '30s', target: 60 },
-    { duration: '30s', target: 80 },
-    { duration: '30s', target: 0 },
+    { duration: '20s', target: 20 },
+    { duration: '40s', target: 60 },
+    { duration: '40s', target: 100 },
+    { duration: '40s', target: 150 },
+    { duration: '40s', target: 0 },
   ],
 };
 
