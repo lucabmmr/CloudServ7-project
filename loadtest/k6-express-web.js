28394fa72787e5062b8e2a05f85fe49d2c55e0ab
Lena111Mx <lena.von.haimbach@gmail.com>
2026-02-11 13:03:33 +0100

    hpa normalisiert, dafür tests angepasst

--- loadtest/k6-express-web.js
+++ loadtest/k6-express-web.js
@@ -1,16 +1,14 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1m', target: 10 },
-    { duration: '2m', target: 50 },
-    { duration: '1m', target: 100 },
-    { duration: '2m', target: 0 },
+    { duration: '20s', target: 0 },
+    { duration: '30s', target: 150 },
+    { duration: '30s', target: 200 },
+    { duration: '30s', target: 0 },
   ],
 };
 
 export default function () {
   http.get('http://10.32.6.161');
-  sleep(1);
 }
